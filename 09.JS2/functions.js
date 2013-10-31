34db769fe3e489fe2bd45277d5192d08815d5756
smo82 <smo@gbg.bg>
2013-11-01 00:01:01 +0200

    JS2

---
+++ 09.JS2/functions.js
@@ -0,0 +1,84 @@
+(function() {
+    if (!Storage.prototype.setObject) {
+        Storage.prototype.setObject = function setObject(key, obj) {
+            var jsonObj = JSON.stringify(obj);
+            this.setItem(key, jsonObj);
+        };
+
+    }
+    if (!Storage.prototype.getObject) {
+        Storage.prototype.getObject = function getObject(key) {
+            var jsonObj = this.getItem(key);
+            var obj = JSON.parse(jsonObj);
+            return obj;
+        };
+    }
+})();
+
+var cookies = (function() {
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function(str) {
+            if (this.length < str.length) {
+                return false;
+            }
+            for (var i = 0; i < str.length; i++) {
+                if (this[i] !== str[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    function readCookie(name) {
+        var allCookies = document.cookie.split(";");
+        for (var i = 0; i < allCookies.length; i++) {
+            var cookie = allCookies[i];
+            var trailingZeros = 0;
+            for (var j = 0; j < cookie.length; j++) {
+                if (cookie[j] !== " ") {
+                    break;
+                }
+            }
+            cookie = cookie.substring(j);
+            if (cookie.startsWith(name + "=")) {
+                return cookie;
+            }
+        }
+    }
+
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        } else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function removeCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+    return {
+        read:readCookie,
+        create:createCookie,
+        remove:removeCookie
+    };
+}());
+
+(function (){
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drag(ev) {
+        ev.dataTransfer.setData("dragged-id", ev.target.id);
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("dragged-id");
+        ev.target.appendChild(document.getElementById(data));
+    }
+})()
